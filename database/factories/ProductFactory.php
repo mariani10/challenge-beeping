aa5e6a5921314368c2441d828eb98d81de60eba0
Mariano <marianowftw@gmail.com>
2024-03-23 07:18:47 +0000

    creación de migrations, seeders, factories y models

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'cost' => fake()->randomFloat(2, 100, 500), // precio random entre 100 y 500
+        ];
+    }
+}
